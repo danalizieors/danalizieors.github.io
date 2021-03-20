8a6d5eee475ab5f9347bc1ffdd7b008a12ce0bcb
Danalizi-Erőss Örs <danalizieors@gmail.com>
2021-03-20 23:13:58 +0200

    Add encoding and decoding with compression for URI components

---
+++ source/store/__tests__/conversion.ts
@@ -0,0 +1,21 @@
+import { decode, encode } from '../conversion'
+
+const object = {
+    null: null,
+    booleans: [false, true],
+    numbers: [2, 34, 567],
+    strings: ['hello', 'world'],
+    object: {
+        field: 'value',
+        nested: {
+            nestedField: 'nestedValue',
+        },
+    },
+}
+
+describe('conversion', () => {
+    test('encoded and decoded object is structurally equal to original', () => {
+        const converted = decode(encode(object))
+        expect(converted).toEqual(object)
+    })
+})
