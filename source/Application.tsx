02fa4c220c944f5b8c2ff69567a7202176f59de6
Danalizi-Erőss Örs <danalizieors@gmail.com>
2021-03-21 21:30:26 +0200

    Change router to HashRouter

--- source/Application.tsx
+++ source/Application.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { Routes } from './Routes'
 import { store } from './store'
 import { theme } from './theme'
